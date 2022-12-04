731983b209268b9c161c5bf8e641ffa72871c1ad
EngineerSahil <77582744+EngineerSahil@users.noreply.github.com>
2022-12-04 23:17:28 +0600

    phone dissmis

--- src/pages/Home/Room/Room.js
+++ src/pages/Home/Room/Room.js
@@ -5,14 +5,14 @@ const Room = () => {
     const {roomId} = useParams();
     const meeting = async(element) =>{
         const appID = 2024379751;
-        const AppId = 2024379751;
-        const AppSign = "f01b47da1b2abd43a821d3b1c09375b57356fc793668b20eb8e3b9aec532e673";
+        // const AppId = 2024379751;
+        // const AppSign = "f01b47da1b2abd43a821d3b1c09375b57356fc793668b20eb8e3b9aec532e673";
         const serverSecret = "b1054c0dabcb1f097c5664659c5a6505";
         const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(
             appID,
             serverSecret,
-            AppId,
-            AppSign,
+            // AppId,
+            // AppSign,
             roomId,
             Date.now().toString(), "sahil"
             );
